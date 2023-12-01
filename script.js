ed51d7bb9e8742902e84d32137c4d310f13ac2a2
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-12-02 03:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+	if(str.charAt(0)==='@' || str.charAt(str.legth-1)==='.'
+	  || !str.includes('@') || !str.includes('.')
+	  ||str.length===0 || str.indexOf('@')>str.indexOf('.'))
+		return false;
+	return true;
+	
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
