ebc65bc2193da3423849a87b845212a8455523e1
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-12-02 03:32:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ function validEmail(str) {
 }
 
 // Do not change the code below.
-// const str = prompt("Enter an email address.");
-// alert(validEmail(str));
+const str = prompt("Enter an email address.");
+alert(validEmail(str));
